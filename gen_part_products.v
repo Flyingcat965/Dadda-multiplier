a1ee261650d993e3f524294ef41f4d207b75d9b5
aswinpajayan <aswinp2008#gmail.com>
2018-11-10 19:27:49 +0530

    finished level 2 , cleaning up order of full adders . bug identified , going to fix

--- gen_part_products.v
+++ gen_part_products.v
@@ -1,3 +1,4 @@
+`timescale 1ps/100fs
 module gen_part_products(
 	input [7:0] A,
 	input [7:0] B,
@@ -5,12 +6,14 @@ module gen_part_products(
 	genvar i;
 	generate
 		for(i = 0; i < 8; i = i +1) begin:part_product
-			assign P[i] = A & B[i] ;
+			assign P[i][0] = A[0] & B[i] ;
+			assign P[i][1] = A[1] & B[i] ;
+			assign P[i][2] = A[2] & B[i] ;
+			assign P[i][3] = A[3] & B[i] ;
+			assign P[i][4] = A[4] & B[i] ;
+			assign P[i][5] = A[5] & B[i] ;
+			assign P[i][6] = A[6] & B[i] ;
+			assign P[i][7] = A[7] & B[i] ;
 		end
 	endgenerate
 endmodule
-
-
-	
-
-
