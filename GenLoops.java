87c80285d7631a7ea44506f88066f3bb90a6face
Aswin P Ajayan <aswinp2008@gmail.com>
2018-11-25 16:49:31 +0530

    Update GenLoops.java
    Commented out the assign statement generation. Ready to generate test data

--- GenLoops.java
+++ GenLoops.java
@@ -4,6 +4,8 @@ public class GenLoops{
 		Random rand = new Random();
 		System.out.println("testing java code");
 		int a = 24,b = 23,c = 5;
+		/*--------------------this part is get some reccuring loops in Dadda multiplier---------------------------
+		
 		for (int fs_index  = 36;fs_index < 47 ; fs_index++){
 			System.out.println("full_adder fa"+fs_index+"(.a(fs["+a+"]),.b(fc["+b+"]),.c_in(fs["+c+"]),.sum(fs["+fs_index+"]),.carry(fc["+fs_index+"]));");
 			a +=1;b+=1;c+=2;
@@ -18,6 +20,7 @@ public class GenLoops{
 			System.out.println("s/fs\\["+a+"\\]/MAC\\[0\\]\\["+b+"\\]/");
 			b++;
 		}
+		----------------------------------------------------------------------------------------------------------------*/
 		int[] ain_array = new int[255];
 		int[] bin_array = new int[255];
 		int[] m_array = new int[255];
